6f8c1198c96cf1a02bb9625432e4edfa1a6304a9
Ifax-wind <aravindr.offi@gmail.com>
2024-02-27 10:46:53 +0530

    -

--- src/Components/NavBar/Option.jsx
+++
@@ -1,9 +0,0 @@
-
-import { Link } from 'react-scroll';
-
-export const Option = (props) => {
-    const{Nav, offset} = props.data;
-    return (
-        <li className='Links'><Link to={Nav} activeClass='active' offset={offset} duration={1000} smooth={true} spy={true} >{Nav}</Link></li>
-    )
-}
